35ef4323a381005d05a1d8b2cf4e21f8ed4c4bba
George-Sava <73170427+George-Sava@users.noreply.github.com>
2022-02-06 08:24:51 -0800

    Update __init__.py

--- jwt/__init__.py
+++ jwt/__init__.py
@@ -25,7 +25,7 @@ from .exceptions import (
 )
 from .jwks_client import PyJWKClient
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 
 __title__ = "PyJWT"
 __description__ = "JSON Web Token implementation in Python"
